4f21743ae146a9d02a1bd0ded4a3963b0f287d35
Williane marques <williane.tads@gmail.com>
2023-04-20 18:19:55 -0300

    Data Fetching no Next.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ import 'keen-slider/keen-slider.min.css'
 
 
 
-export default function Home() {
+export default function Home(props) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 3,
@@ -21,6 +21,7 @@ export default function Home() {
   
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
+      <pre>{JSON.stringify(props.list)}</pre>
     <Product className="keen-slider__slide">
       <Image src={camiseta1} width={520} height={480} alt="" />
 
@@ -59,4 +60,16 @@ export default function Home() {
 
   </HomeContainer>
   )
+}
+
+export const getServerSideProps = async () => {
+  await new Promise(resolve => setTimeout(resolve, 2000))
+
+  console.log('rodou')
+
+  return {
+    props: {
+      list: [1,2,3]
+    }
+  }
 }
